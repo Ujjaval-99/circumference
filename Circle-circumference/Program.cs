38c09e273d92baebfe90bc5c671da8e8acf5ef3c
Ujjaval-99 <ujjavalmistry99@gmail.com>
2023-05-09 11:11:56 +0530

    Initial commit

---
+++ Circle-circumference/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Program to implement area of circle in C#
+using System;
+class Circle
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter Radius: ");
+        double rad = Convert.ToDouble(Console.ReadLine());
+        double area = Math.PI * rad * rad;
+        Console.WriteLine("Area of circle is: " + area);
+    }
+}
